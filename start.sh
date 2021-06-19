bda1d5e94ad46bc025ac2394ab23d8fb9f395729
MMOTutkunlari <gs.eray.55@hotmail.com>
2021-06-19 21:25:20 +0300

    +sh arguments
    Oyunu başlatma shell scriptine komut ile argument alınmasını sağlayan düzenlemeler.

--- start.sh
+++ start.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 #### @Whistle | www.mmotutkunlari.com | oyunu baslatma scripti ####
+
+flags=""
+while getopts l:I: flag
+do
+	case ${flag} in
+		l) level=$OPTARG
+			flags=" $flags -l $level "
+			echo "LOG LEVEL: $level"
+			;;
+		I) IP=$OPTARG
+			flags=" $flags -I $IP "
+			echo "PUBLIC IP: $IP"
+			;;
+		?) echo ""
+			echo "Kullanım: $0 -l log_level -I public_ip"
+			echo -e "\t-l : Serverin log seviyesini belirlemek için 0'dan 3'e kadar değer alabilir."
+			echo -e "\t-I : Serverin PUBLIC_IP değerini belirlemek içindir."
+			exit 1
+			;;
+	esac
+done
+
 main_dir=$(pwd)
 cores="db auth $(cat cores.list)"
 for i in $cores
@@ -8,7 +30,7 @@ do
 	if [ -z $PIDNUM ]; then
 		echo "$i aciliyor..."
 		cd $main_dir/cores/$i
-		./$i & pwd & sleep 5
+		./$i $flags & pwd & sleep 5
 	else
 		echo "$i zaten acik. pid: $PIDNUM"
 	fi
